7fa3143c6d3cfb05bc01e2537288225e8bfdd1e8
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-06-24 21:43:53 +0530

    EmptyDelete added

---
+++ EmptyDelete.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int fun() {
+
+  return 0;
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
