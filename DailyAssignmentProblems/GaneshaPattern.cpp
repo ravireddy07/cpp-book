36da480ff50bbd30d4e0d1eea441875848f1bcc6
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-07-02 02:04:27 +0530

    New Assignment added

--- DailyAssignmentProblems/GaneshaPattern.cpp
+++ DailyAssignmentProblems/GaneshaPattern.cpp
@@ -14,45 +14,34 @@ int main () {
 
   cout<<endl;
 
-  for(i=1;i<=(n+1)/2 - 2;i++) {
+    for(i=1;i<=(n+1)/2 - 2;i++) {
         cout << "*";
-
-        for (j = 1; j <= (n+1)/2 - 2; j++)
-        cout << " ";
-
-        cout << "*";
-
-        cout << endl;
+        for (j=1;j<=(n+1)/2 - 2;j++)
+        cout<<" ";
+        cout<<"*";
+        cout<<endl;
     }
 
-    //work for middle row
-    for (i = 1; i <= n; i++)
-        cout <<"*";
-    cout << endl;
+    for(i=1;i<=n;i++)
+        cout<<"*";
+    cout<<endl;
 
-    //work till last row
-    for (i = 1; i <= (n+1)/2 - 2; i++)
-    {
-        for (j = 1; j <= (n+1)/2 - 1; j++)
-        cout << " ";
-
-        cout << "*";
-
-        for (j = 1; j <= (n+1)/2 - 2; j++)
-        cout << " ";
-
-        cout << "*";
-
-        cout << endl;
+    for(i=1;i<=(n+1)/2 - 2;i++) {
+        for(j=1;j<=(n+1)/2 - 1;j++)
+        cout<<" ";
+        cout<<"*";
+        for(j=1;j<=(n+1)/2 - 2;j++)
+        cout<<" ";
+        cout<<"*";
+        cout<<endl;
     }
 
-    //work of last row
-    for (i = 1; i <= (n+1)/2; i++)
-        cout << "*";
+    for(i=1;i<=(n+1)/2;i++)
+        cout<<"*";
 
-    for (i = 1; i <= (n+1)/2 - 2; i++)
-        cout << " ";
+    for(i=1;i<=(n+1)/2 - 2;i++)
+        cout<<" ";
 
-    cout <<"*";
+    cout<<"*";
     return 0;
 }
