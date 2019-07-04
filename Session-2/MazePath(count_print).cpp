81d1142f36d2f436df9fae0845f059a7e366bfd1
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-07-04 22:21:36 +0530

    New Assignments solutions added

--- Session-2/MazePath(count_print).cpp
+++ Session-2/MazePath(count_print).cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int result = 0;
+
 void countMazePath(int endrow, int endcol, int curr_row, int curr_col, string osf) {
 	// base case
 	if(curr_row == endrow and curr_col == endcol) {
-		cout<<osf<<endl;
+		cout<<osf<<" ";
+        result++;
 		return ;
 	}
 	if(curr_row>endrow or curr_col>endcol) {
 		return ;
 	}
-
 	countMazePath(endrow, endcol, curr_row+1, curr_col, osf+"V");
 	countMazePath(endrow, endcol, curr_row, curr_col+1, osf+"H");
 }
 
 int main(int argc, char const *argv[])
 {
-	countMazePath(2, 2, 0, 0, "");
+	/* code */
+	int row, col;
+	cin>>row;
+	cin>>col;
+	countMazePath(row-1, col-1, 0, 0, "");
+    cout<<endl<<result;
 	return 0;
 }
