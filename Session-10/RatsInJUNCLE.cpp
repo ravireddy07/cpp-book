c893dadcaeae543a63a6ff514d798916d89867c3
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-07-15 19:55:24 +0530

    Session-10 Updated

---
+++ Session-10/RatsInJUNCLE.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+int Jungle(int *arr, int n) {
+    unordered_map<int, int > mp;
+    int result = 0;
+    for(int i=0;i<n;i++) {
+        if(arr[i] == 0) {
+            result +=1;
+            continue;
+        }
+        if(mp.find(arr[i]) == mp.end()) {
+            result += (arr[i]+1);
+            mp[arr[i]] = 0;
+        }
+        else {
+            mp[arr[i]]++;
+            if(mp[arr[i]] == arr[i]) {
+                mp.erase(arr[i]);
+            }
+        }
+    }
+  return result;
+}
+
+
+int main(int argc, char const *argv[]) {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    cout<<Jungle(arr, n);
+    return 0;
+}
