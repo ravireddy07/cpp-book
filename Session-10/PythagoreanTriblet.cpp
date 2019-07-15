789ddfaefd004420f20f0b3c01879cc0624c5c25
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-07-15 19:54:26 +0530

    Session-10 added

---
+++ Session-10/PythagoreanTriblet.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+/*
+[](m^2 - n^2), (2mn), (m^2 + n^2)]
+*/
+int Pythogorus(int x) {
+
+  return 0;
+}
+
+
+int main(int argc, char const *argv[]) {
+  int x;
+  cin>>x;
+
+  cout<<Pythogorus(x)<<endl;
+  return 0;
+}
