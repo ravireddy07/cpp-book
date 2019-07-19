84cc4778c68090c3e7363d1e50b46a1892ac98a7
ravireddy07 <ravikanthreddy8500@gmail.com>
2019-07-19 15:32:55 +0530

    season-12 addded

---
+++ Session-12/ModularExponentation.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long int
+
+ll PowerExp(ll a, ll b, ll c) {
+	ll result = 1;
+	while(b>0) {
+		if(b&1) {
+			result = result*a;
+			result = result % c;
+			result = (result+c) % c;
+			a=a*a;
+			a=a%c;
+			b=b>>1;
+		}
+		return result%c;
+}
+
+ll ModularExp(ll a, ll b, ll c) {
+  return PowerExp(a,b,c)%c;
+}
+
+int main() {
+  ll a,b,c;
+  cin>>a>>b>>c;
+  cout<<ModularExp(a,b,c);
+	return 0;
+}
