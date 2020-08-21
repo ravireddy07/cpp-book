66b4d320ab761445543dcefc39360f9936383aeb
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2020-08-21 23:10:42 +0530

    daily task added

--- Leetcode/SortArrayByParity.cpp
+++ Leetcode/SortArrayByParity.cpp
@@ -1,3 +1,7 @@
+//  Discussion thread Link:
+
+//  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3431/discuss/804079/C++-Simple-Solution-or-Top-98.84-in-12-ms-Runtime-or-12ms-and-60ms-Solution-Difference
+
 #include <iostream>
 #include <vector>
 using namespace std;
---
+++ Leetcode/WildcardMatching.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isMatch(string a, string b)
+    {
+        int n = a.size(), m = b.size(), i, j;
+        int dp[n + 1][m + 1];
+        memset(dp, 0, sizeof dp);
+        dp[0][0] = 1;
+
+        for (j = 1; j <= m; j++)
+        {
+            if (b[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                if (b[j - 1] == '*')
+                {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else if (b[j - 1] == '?' || a[i - 1] == b[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+int main()
+{
+    Solution a;
+    bool result = a.isMatch("aa", "a");
+    cout << result << endl;
+}
