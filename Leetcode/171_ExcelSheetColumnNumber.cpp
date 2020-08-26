5a7e5e9f1cc20f043ece9a34c7db0edc587de505
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2020-08-26 10:57:10 +0530

    daily task

---
+++ Leetcode/171_ExcelSheetColumnNumber.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/excel-sheet-column-number/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int titleToNumber(string s)
+    {
+        int result = 0, comb = s.size();
+        for (int i = comb - 1; i >= 0; i--)
+            result += pow(26, comb - i - 1) * ((int)s[i] - 64);
+        return result;
+    }
+};
+
+int main()
+{
+    Solution a;
+    cout << a.titleToNumber("AX") << endl;
+    return 0;
+}
---
+++ Leetcode/274_H-Index.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/h-index/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        //sort the array and scan the array
+        int size = citations.size();
+        if (size == 0)
+            return 0;
+
+        sort(citations.begin(), citations.end(), std::greater<int>());
+        int i = 0;
+        while (i < size && citations[i] >= i + 1)
+        {
+            i++;
+        }
+
+        return i;
+    }
+};
+
+int main()
+{
+    Solution a;
+    vector<int> input = {3, 0, 6, 1, 5};
+    cout << a.hIndex(input) << endl;
+    return 0;
+}
