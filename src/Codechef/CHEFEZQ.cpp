9e02315a9097d21896a846d8cd7afe55060d16e5
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2020-10-02 21:48:51 +0530

    new problem

--- src/Codechef/CHEFEZQ.cpp
+++ src/Codechef/CHEFEZQ.cpp
@@ -1,31 +1,80 @@
 // https://www.codechef.com/OCT20B/problems/CHEFEZQ
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 void test()
 {
     int n, k;
-    scanf("%d%d", &n, &k);
+    //scanf("%d%d", &n, &k);
+    cin >> n >> k;
     vector<int> q(n);
     int sum = 0;
     for (int i = 0; i < n; ++i)
     {
-        scanf("%d", &q[i]);
+        //scanf("%d", &q[i]);
+        cin >> q[i];
         sum += q[i];
     }
-
-    double res = sum / k;
-    cout << (int)res + 1 << endl;
+    cout << sum / k + 1 << endl;
     return;
 }
+
+void test1()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    int rem = 0;
+    int days = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        rem += v[i] - k;
+        if (rem < 0)
+        {
+            cout << days << endl;
+            return;
+        }
+        days++;
+    }
+    cout << days + (rem / k) << endl;
+}
+
+// https://www.codechef.com/OCT20B/problems/CHEFEZQ
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        test();
+        int n, k;
+        cin >> n >> k;
+        int v[n];
+        for (int i = 0; i < n; ++i)
+            cin >> v[i];
+
+        int rem = 0;
+        int days = 1;
+        for (int i = 0; i < n; ++i)
+        {
+            rem += v[i] - k;
+            if (rem < 0)
+            {
+                cout << days << endl;
+                return;
+            }
+            days++;
+        }
+        cout << days + (rem / k) << endl;
     }
     return 0;
 }
