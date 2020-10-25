e24094ff8d6f2a52d4c39efeb04d4e8044208dbe
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2020-10-25 13:58:55 +0530

    update #1436

--- src/Codeforces/PrimeSquares.cpp
+++ src/Codeforces/PrimeSquares.cpp
@@ -4,76 +4,34 @@
 #define ll long long int
 using namespace std;
 
-bool isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
 void test()
 {
     int n;
     cin >> n;
-    vector<int> res;
-    int c = 1, k = 1, sum = 1, temp = 1;
-
-    while (c != n)
-    {
-        if (k == 1)
-            res.push_back(k);
-        else if (!isPrime(k))
-        {
-            temp += k;
-            if (isPrime(temp))
-            {
-                sum += k;
-                res.push_back(k);
-                c++;
-            }
-            temp = sum;
-        }
-        k++;
-    }
-
-    for (int i = 0; i < res.size(); i++)
-    {
-        for (int j = i; j < res.size(); j++)
-            cout << res[j] << " ";
-        for (int j = i - 1; j >= 0; j--)
-            cout << res[j] << " ";
-        cout << endl;
-    }
-    return;
-}
-
-void test1()
-{
-    int n;
-    cin >> n;
-    vector<int> res(n);
-    int rsum = 3;
     for (int i = 0; i < n; i++)
     {
-        if (rsum > 0)
+        for (int j = 0; j < n; j++)
         {
-            res[i] = 1;
-            rsum--;
+            if (i == j || i == j - 1 || i == j + 1)
+            {
+                cout << "1"
+                     << " ";
+            }
+            else
+            {
+                cout << "0"
+                     << " ";
+            }
         }
-        else
-            res[i] = 0;
-    }
-    for (int i = 0; i < res.size(); i++)
-    {
-        for (int j = i; j < res.size(); j++)
-            cout << res[j] << " ";
-        for (int j = i - 1; j >= 0; j--)
-            cout << res[j] << " ";
         cout << endl;
     }
+    /*
+    1 1 0 0 0
+    1 1 1 0 0
+    0 1 1 1 0
+    0 0 1 1 1
+    0 0 0 1 1
+    */
     return;
 }
 
