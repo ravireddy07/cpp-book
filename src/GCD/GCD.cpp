d3ad06434ebde241d8d5a9611cfb1634e4f8aeea
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2020-10-31 19:21:24 +0530

    update

--- src/GCD/GCD.cpp
+++ src/GCD/GCD.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int gcd(int a,int b){
-    return b==0?a:gcd(b,a%b);
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
 }
 
-int main() {
-    int n1,n2;
-    cin>>n1>>n2;
-    cout<<gcd(n1,n2)<<endl;
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+    cout << gcd(n1, n2) << endl;
     return 0;
 }
