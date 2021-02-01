3989c3cca6b86f5244cde891f787d042ca895cc0
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2021-02-01 20:49:27 +0530

    leetcode_feb_day-1

--- src/Leetcode/191_number-of-1-bits.cpp
+++ src/Leetcode/191_number-of-1-bits.cpp
@@ -1,25 +1,54 @@
 // https://leetcode.com/problems/number-of-1-bits/
 
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int hammingWeight(uint32_t n)
-    {
-        int count = 0;
-        for (int i = 0; i <= 31; i++)
-            count += ((n >> i) & (0x1));
-        return count;
-        //return __builtin_popcount(n);
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while (n) {
+            n &= (n - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+
+    int hammingWeight(uint32_t n) {
+        int cnt = 0; // count of set bit
+        while (n > 0) { // iterate until all bits are traversed
+            if ((n & 1) > 0) // check the parity of first bit from right
+                ++cnt;
+            n = n >> 1; //n=n/2, shift one bit to right
+        }
+        return cnt;
+    }
+
+    int hammingWeight(uint32_t n) {
+        int cnt = 0; // count of set bit
+        ++cnt;
+        n = n & (n - 1); // change the first set bit from right to 0
+    }
+    return cnt;
+
+    int hammingWeight(uint32_t n) {
+        return __builtin_popcountll(n);
     }
 };
 
 int main()
 {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     Solution a;
-    cout << a.hammingWeight(00000000000000000000000000001011) << endl;
+    int t;
+    cin >> t;
+    while (t--) {
+        uint32_t in;
+        cin >> in;
+        cout << a.hammingWeight(in) << "\n";
+    }
     return 0;
 }
