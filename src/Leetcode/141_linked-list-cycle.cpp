5c15971500cf25f60c88e8ad95819756e815fa6d
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2021-02-03 22:02:00 +0530

    lc feb-3

---
+++ src/Leetcode/141_linked-list-cycle.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head)
+            return false;
+
+        ListNode *slow = head, *fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
