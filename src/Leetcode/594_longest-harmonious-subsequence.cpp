56d82b87c58282356b9cb52dccd60f4573969e9b
Ravi Kanth Gojur <ravikanthreddy8500@gmail.com>
2021-02-04 22:20:45 +0530

    lc feb-4

--- src/Leetcode/594_longest-harmonious-subsequence.cpp
+++ src/Leetcode/594_longest-harmonious-subsequence.cpp
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/longest-harmonious-subsequence/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 public:
+    // easy
     int findLHS(vector<int> &nums)
     {
         unordered_map<int, int> um;
@@ -21,6 +23,38 @@ public:
             }
         }
         return ans;
+    }
+
+    // Brute Force
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> cnt;
+        int res = 0, cntr = 1;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1]) {
+                cntr++;
+            }
+            else {
+                cnt.push_back(cntr);
+                cntr = 1;
+            }
+        }
+        cnt.push_back(cntr);
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1]) {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+
+        for (int i = 1; i < nums.size(); i++)
+            if ((abs(nums[i] - nums[i - 1]) == 0 or abs(nums[i] - nums[i - 1]) == 1) and (cnt[i] + cnt[i - 1] > res))
+                res = cnt[i] + cnt[i - 1];
+
+        return res;
     }
 };
 
