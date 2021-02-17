f3e30d8f1436e7359f3c821b463578d577145a43
ravireddy07 <ravikanthreddy8500@gmail.com>
2021-02-17 21:34:12 +0530

    update

---
+++ src/Code-Forces/maximum-gcd.cpp
@@ -0,0 +1,15 @@
+// Problem Link: https://codeforces.com/problemset/problem/1370/A
+
+#include <bits/stdc++.h>
+
+int main()
+{
+	int t, n;
+	scanf("%d", &t);
+	while (t--) 
+	{
+		scanf("%d", &n);
+		printf("%d\n", n / 2);
+	}
+	return 0;
+}
