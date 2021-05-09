5d36918182620da28ea9e8e2393f0e8f29a39af9
ravireddy07 <ravikanthreddy8500@gmail.com>
2021-05-09 17:02:53 +0530

    cc long #2

---
+++ src/Code-Chef/MAY2021-Long/TCTCTOE.cpp
@@ -0,0 +1,211 @@
+// Problem Statement:
+
+/**
+ *  Author: Ravi Kanth Gojur
+ *  Code Forces: ravireddy07
+ *  Code Chef: ravireddy115
+ *  Github: ravireddy07
+**/
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define ravireddy07 return
+#define ii(a) scanf("%d", &a)
+#define ii2(a, b) scanf("%d%d", &a, &b)
+#define ii3(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define ill(a) scanf("%lld", &a)
+#define ill2(a, b) scanf("%lld%lld", &a, &b)
+#define ill3(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+#define for1(i, a, b) for (int i = a; i < b; ++i)
+#define for2(i, a, b) for (int i = b; i >= a; --i)
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vl vector<ll>
+#define vll vector<vector<ll>>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define mp make_pair
+#define unmp unordered_map
+#define pq priority_queue
+#define pb push_back
+#define sorta(a) sort(a.begin(), a.end())
+#define sortd(a) sort(a.begin(), a.end(), greater<>())
+#define sortr(a) sort(a.rbegin(), a.rend())
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define setValue(a, value) memset(a, value, sizeof(a))
+using namespace std;
+ll MOD = 1e9 + 7;
+
+template <typename T, typename T1>
+T amax(T &a, T1 b)
+{
+    if (b > a)
+        a = b;
+    return a;
+}
+
+template <typename T, typename T1>
+T amin(T &a, T1 b)
+{
+    if (a > b)
+        a = b;
+    return a;
+}
+
+/*
+vector<int> getPermutation(size_t n)
+{
+	vector<int> res;
+	string s = to_string(n);
+	sort(s.begin(), s.end());
+	do
+	{
+		res.pb(stoi(s));
+		//cout << s << "\n";
+	} while (getPermutation(s.begin(), s.end()));
+	return res;
+}*/
+
+ll fpow(ll base, ll power)
+{
+    ll result = 1;
+    while (power > 0)
+    {
+        if (power & 1)
+            result = (result * base) % MOD;
+        base = (base * base) % MOD;
+        //power >> = 1;
+        power = power / 2;
+    }
+    ravireddy07 result;
+}
+
+void dfs(ll s, vector<vector<ll>> &adj, vector<bool> &vis, vector<ll> &arr)
+{
+    if (vis[s])
+        return;
+    vis[s] = true;
+    arr.pb(s);
+    for (auto x : adj[s])
+        if (vis[x] == false)
+            dfs(x, adj, vis, arr);
+    ravireddy07;
+}
+
+void bfs(ll x, vector<bool> &vis, vector<vector<ll>> &adlist, vector<ll> &level, vector<ll> &parent)
+{
+    queue<ll> q;
+    vis[x] = true;
+    q.push(x);
+    while (!q.empty())
+    {
+        ll s = q.front();
+        q.pop();
+        // process node s
+        for (auto u : adlist[s])
+        {
+            if (vis[u])
+                continue;
+            vis[u] = true;
+            parent[u] = s;
+            level[u] = level[s] + 1;
+            q.push(u);
+        }
+    }
+    ravireddy07;
+}
+
+void harry()
+{
+    char ar[3][3];
+    int underScore = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> ar[i][j];
+            if (ar[i][j] == '_')
+                underScore++;
+        }
+    }
+
+    int xo = 0;
+    // checking X-O
+    // 1
+    //cout<< ar[0][0] <<" "<< ar[0][1] << " "<< ar[0][2] <<"\n";
+    if (ar[0][0] == ar[0][1] and ar[0][1] == ar[0][2])
+    {
+        xo++;
+    }
+    if (ar[0][0] == ar[1][0] and ar[1][0] == ar[2][0])
+    {
+        xo++;
+    }
+
+    // 2
+    if (ar[0][1] == ar[1][1] and ar[1][1] == ar[2][1])
+    {
+        xo++;
+    }
+    if (ar[1][0] == ar[1][1] and ar[1][1] == ar[1][2])
+    {
+        xo++;
+    }
+
+    // 3
+    if (ar[2][2] == ar[2][1] and ar[2][1] == ar[2][0])
+    {
+        xo++;
+    }
+    if (ar[0][2] == ar[1][2] and ar[1][2] == ar[2][2])
+    {
+        xo++;
+    }
+
+    // diagonal
+    if (ar[0][0] == ar[1][1] and ar[1][1] == ar[2][2])
+    {
+        xo++;
+    }
+    if (ar[0][2] == ar[1][1] and ar[1][1] == ar[2][0])
+    {
+        xo++;
+    }
+
+    if (xo == 0 and underScore == 0)
+        cout << 1 << "\n";
+    else if (xo > 1)
+    {
+        cout << 3 << "\n";
+    }
+    else if (xo == 1)
+    {
+        cout << 1 << "\n";
+    }
+    else
+    {
+        cout << 2 << "\n";
+    }
+}
+/*
+XOX
+O_O
+XOX
+*/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    ii(t);
+    while (t--)
+        harry();
+    return 0;
+}
