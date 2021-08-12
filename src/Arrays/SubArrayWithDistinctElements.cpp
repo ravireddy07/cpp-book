0ca73b05f676ed4b33a9e83985cae57703e8c756
ravireddy07 <ravikanthreddy8500@gmail.com>
2021-08-12 22:24:53 +0530

    update

--- src/Arrays/SubArrayWithDistinctElements.cpp
+++ src/Arrays/SubArrayWithDistinctElements.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sumOflength(int *arr, int n)
+int sumOflength(int arr, int n)
 {
     unordered_set<int> s;
     int j = 0, ans = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
     {
         while (j < n and s.find(arr[j]) == s.end())
         {
@@ -20,7 +20,6 @@ int sumOflength(int *arr, int n)
 
 int main()
 {
-    int arr[] = {1, 4, 2, 6, 8, 4, 9, 3, 2, 8};
-    cout << sumOflength(arr, sizeof(arr) / sizeof(arr[0])) << endl;
+    cout << sumOflength({1, 4, 2, 6, 8, 4, 9, 3, 2, 8}, sizeof(arr) / sizeof(arr[0])) << "\n";
     return 0;
 }
--- src/Code-Chef/VOTERS.cpp
+++ src/Code-Chef/VOTERS.cpp
@@ -1,23 +1,44 @@
-#include<cstdio>
-#include<set>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-
+int main()
+{
     int firstSize, secondSize, thirdSize;
     scanf("%d %d %d", &firstSize, &secondSize, &thirdSize);
 
     std::set<int> first, second, third, output;
-    for(int k = 0; k < firstSize; k++){int temp; scanf("%d", &temp); first.insert(temp);}
-    for(int k = 0; k < secondSize; k++){int temp; scanf("%d", &temp); second.insert(temp);}
-    for(int k = 0; k < thirdSize; k++){int temp; scanf("%d", &temp); third.insert(temp);}
+    int temp;
+    for (int k = 0; k < firstSize; k++)
+    {
 
+        scanf("%d", &temp);
+        first.insert(temp);
+    }
+    for (int k = 0; k < secondSize; k++)
+    {
+        scanf("%d", &temp);
+        second.insert(temp);
+    }
+    for (int k = 0; k < thirdSize; k++)
+    {
+        scanf("%d", &temp);
+        third.insert(temp);
+    }
 
     std::set<int>::iterator setIter;
-    for(setIter = first.begin(); setIter != first.end(); setIter++){if(second.find(*setIter) != second.end() || third.find(*setIter) != third.end()){output.insert(*setIter);}}
-    for(setIter = second.begin(); setIter != second.end(); setIter++){if(third.find(*setIter) != third.end()){output.insert(*setIter);}}
+    for (setIter = first.begin(); setIter != first.end(); setIter++)
+        if (second.find(*setIter) != second.end() || third.find(*setIter) != third.end())
+            output.insert(*setIter);
+
+    for (setIter = second.begin(); setIter != second.end(); setIter++)
+        if (third.find(*setIter) != third.end())
+            output.insert(*setIter);
 
     printf("%ld\n", output.size());
-    for(setIter = output.begin(); setIter != output.end(); setIter++){printf("%d\n", *setIter);}; puts("");
 
+    for (setIter = output.begin(); setIter != output.end(); setIter++)
+        printf("%d\n", *setIter);
+
+    puts("");
     return 0;
 }
