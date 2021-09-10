9f2f3d63a7efa3f402f28091f07bdaa1b9c5d347
ravireddy07 <ravikanthreddy8500@gmail.com>
2021-09-10 22:18:17 +0530

    update

--- src/Leetcode/905_sort-array-by-parity.cpp
+++ src/Leetcode/905_sort-array-by-parity.cpp
@@ -1,41 +1,33 @@
-//  https://leetcode.com/problems/sort-array-by-parity/
-//  https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3431/discuss/804079/C++-Simple-Solution-or-Top-98.84-in-12-ms-Runtime-or-12ms-and-60ms-Solution-Difference
+// Problem Statement: https://leetcode.com/problems/sort-array-by-parity/
+// https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3431/discuss/804079/C++-Simple-Solution-or-Top-98.84-in-12-ms-Runtime-or-12ms-and-60ms-Solution-Difference
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 class sortArrayByParity
 {
 public:
-    vector<int> Method_1(vector<int> &A)
+    vector<int> Method_1(vector<int> A)
     {
         vector<int> result;
         //result.reserve(A.size());
-
         for (int i = 0; i < A.size(); i++)
         {
             if (A[i] % 2 == 0)
-            {
                 result.insert(result.begin(), A[i]);
-            }
             else
-            {
                 result.insert(result.end(), A[i]);
-            }
         }
         return result;
     }
 
-    vector<int> Method_2(vector<int> &A)
+    vector<int> Method_2(vector<int> A)
     {
         int size = A.size(), start = 0, end = size - 1;
         while (start < end)
         {
             if (A[start] % 2 == 0)
-            {
                 start++;
-            }
             else
             {
                 swap(A[start], A[end]);
@@ -45,24 +37,17 @@ public:
         return A;
     }
 
-    vector<int> Method_3(vector<int> &A)
+    vector<int> Method_3(vector<int> A)
     {
         vector<int> result;
         //result.reserve(A.size());
-        for (int i = 0; i < A.size(); i++)
-        {
+        for (int i = 0; i < A.size(); ++i)
             if (A[i] % 2 == 0)
-            {
                 result.push_back(A[i]);
-            }
-        }
-        for (int j = 0; j < A.size(); j++)
-        {
+
+        for (int j = 0; j < A.size(); ++j)
             if (A[j] % 2 != 0)
-            {
                 result.push_back(A[j]);
-            }
-        }
         return result;
     }
 };
@@ -70,23 +55,18 @@ public:
 int main()
 {
     vector<int> show;
-    vector<int> input = {3, 1, 2, 4};
     sortArrayByParity a;
-    show = a.Method_1(input); //Runtime: 60 ms
-    show = a.Method_2(input); //Runtime: 16 ms
-    show = a.Method_3(input); //Runtime: 12 ms
+    show = a.Method_1({3, 1, 2, 4}); //Runtime: 60 ms
+    show = a.Method_2({3, 1, 2, 4}); //Runtime: 16 ms
+    show = a.Method_3({3, 1, 2, 4}); //Runtime: 12 ms
 
     cout << "[";
     for (int i = 0; i < show.size(); i++)
     {
         if (i == show.size() - 1)
-        {
             cout << show[i];
-        }
         else
-        {
             cout << show[i] << ", ";
-        }
     }
     cout << "]";
 }
